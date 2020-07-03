fix transition rate calculation

FRETboard/algorithms/VanillaHmm.py
@@ -4,8 +4,9 @@ import pomegranate as pg
 from pomegranate.kmeans import Kmeans
 from random import choices
 import itertools
+from itertools import permutations
 import yaml
-from FRETboard.helper_functions import numeric_timestamp
+from FRETboard.helper_functions import numeric_timestamp, discrete2continuous
 
 
 def parallel_predict(tup_list, mod):
@@ -27,6 +28,7 @@ class Classifier(object):
         :param buffer: int, size of buffer area around regular classes [required if gui not given]
         """
         self.trained = None
+        self.framerate = None
         self.timestamp = numeric_timestamp()
         self.nb_threads = kwargs.get('nb_threads', 8)
         self.feature_list = kwargs['features']
@@ -50,6 +52,10 @@ class Classifier(object):
         self.timestamp = numeric_timestamp()
 
     def get_trained_hmm(self, data_dict, bootstrap=False):
+
+        if self.framerate is None:
+            self.framerate = 1 / np.concatenate([data_dict[tr].time.iloc[1:].to_numpy() -
+                                                 data_dict[tr].time.iloc[:-1].to_numpy() for tr in data_dict]).mean()
 
         # Make selection of tuples, if bootstrapping (otherwise use all data)
         if bootstrap:
@@ -79,8 +85,14 @@ class Classifier(object):
                     labels.append(None)
             # labels = [[hmm.start.name] + list(data_dict[dd].labels) + [hmm.end.name]
             #           if self.data.manual_table.loc[dd, 'is_labeled'] else None for dd in data_dict]
+
             nsi = 1.0 - self.supervision_influence
             weights = [nsi if lab is None else self.supervision_influence for lab in labels]
+            # nb_labeled = self.data.manual_table.is_labeled.sum()
+            # si = nb_labeled / len(self.data.index_table)
+            # nsi = 1 - si
+            # weights = [nsi if lab is None else si for lab in labels]
+
             hmm.fit(X, weights=weights, labels=labels, use_pseudocount=True, algorithm='viterbi')
         elif not any(self.data.manual_table.is_labeled):
             hmm.fit(X, use_pseudocount=True, algorithm='viterbi')
@@ -226,11 +238,14 @@ class Classifier(object):
     # --- parameters and performance measures ---
     def get_data_tm(self, trace_dict, out_labels, nb_bootstrap_iters):
         """
-        Calculate bootstrapped confidence intervals on data-derived transition matrix values
-        :return:
+        Calculate bootstrapped confidence intervals on data-derived transition matrix values and convert to
+        continous transition rates
         """
-        # actual value
-        actual_tm = self.tm_from_seq(out_labels)
+        state_order_dict = {state.name: idx for idx, state in enumerate(self.trained.states)}
+
+        # actual estimate
+        actual_tm = self.tm_from_hmm(self.trained, state_order_dict)
+        # actual_tm = discrete2continuous(self.tm_from_seq(out_labels), self.framerate)
 
         # CIs
         tm_array = []
@@ -244,8 +259,11 @@ class Classifier(object):
         trace_dict = {tr: trace_dict[tr] for tr in trace_dict if tr in idx_list}
         for _ in range(nb_bootstrap_iters):
             hmm = self.get_trained_hmm(trace_dict, bootstrap=True)
-            seqs = [self.predict(trace_dict[idx], hmm)[0] for idx in idx_list]
-            tm_array.append(self.tm_from_seq(seqs))
+            tm = self.tm_from_hmm(hmm, state_order_dict)
+            tm_array.append(tm)
+            # seqs = [self.predict(trace_dict[idx], hmm)[0] for idx in idx_list]
+            # cur_tm = discrete2continuous(self.tm_from_seq(seqs), self.framerate)
+            # tm_array.append(cur_tm)
         tm_mat = np.stack(tm_array, axis=-1)
         sd_mat = np.std(tm_mat, axis=-1)
         mu_mat = np.mean(tm_mat, axis=-1)
@@ -253,6 +271,16 @@ class Classifier(object):
         ci_mat[:, :, 0] -= sd_mat * 2
         ci_mat[:, :, 1] += sd_mat * 2
         return actual_tm, ci_mat
+
+    def tm_from_hmm(self, hmm, state_order_dict):
+        full_tm = hmm.dense_transition_matrix()
+        tm = np.zeros([self.nb_states, self.nb_states])
+        state_list = list(range(self.nb_states))
+        for s1, s2 in permutations(state_list, 2):
+            tm[s1, s2] = full_tm[state_order_dict[f's{s1}'], state_order_dict[f's{s2}']]
+        for s in state_list:
+            tm[s,s] = full_tm[state_order_dict[f's{s}'], state_order_dict[f's{s}']]
+        return discrete2continuous(tm, self.framerate)
 
     def tm_from_seq(self, seq_list):
         # concatenate seqs with stop symbol in between

not_for_upload/FretReport_sim.py
@@ -1,6 +1,7 @@
 import os
 import io
 import numpy as np
+from scipy.linalg import logm
 
 import matplotlib
 matplotlib.use('Agg')
@@ -127,12 +128,13 @@ class FretReport(object):
         tm_vec, ci_vecs = self.classifier.get_data_tm(self.tr_dict, self.out_labels,
                                                       self.bootstrap_size)
 
-        # translate prob to transition rate
-        for s in np.arange(self.classifier.nb_states):
-            tm_vec[s, s] -= 1
-            ci_vecs[s, s, :] -= 1
-        tm_vec *= self.frame_rate
-        ci_vecs *= self.frame_rate
+        # # Correct to continous transition rate
+        # tm_vec = np.eye(self.classifier.nb_states) + self.frame_rate * logm(tm_vec)
+        # for s in np.arange(self.classifier.nb_states):
+        #     tm_vec[s, s] -= 1
+        #     ci_vecs[s, s, :] -= 1
+        # # tm_vec *= self.frame_rate
+        # # ci_vecs *= self.frame_rate
 
         # make df for csv file
         state_list = [str(nb + 1) for nb in range(self.classifier.nb_states)]

not_for_upload/evaluate_traces.py
@@ -393,19 +393,21 @@ if tr_files:
     transition_df.loc[:, 'rate'] = 0
     tb = transition_df.nb_transitions != 0
 
+    # transition_df.loc[tb, 'rate'] = transition_df.loc[tb, 'nb_transitions'] / transition_df.loc[tb, 'nb_samples'] * framerate
+
     # correct discrete --> continuous
-    # tm = np.zeros((nb_classes, nb_classes))
-    # for ti, tup in transition_df.iterrows():
-    #     tm[int(ti[0]) - 1, int(ti[1]) - 1] = tup.nb_transitions / tup.nb_samples
-    # for i in range(nb_classes):
-    #     tm[i,i] = 1 - np.sum(tm[i,:])
+    tm = np.zeros((nb_classes, nb_classes))
+    for ti, tup in transition_df.iterrows():
+        s1, s2 = [int(tii) - 1 for tii in ti.split('_')]
+        tm[s1, s2] = tup.nb_transitions / tup.nb_samples
+    for i in range(nb_classes):
+        tm[i,i] = 1 - np.sum(tm[i,:])
 
-    # tm_rate = np.eye(nb_classes) + framerate * logm(tm)
-    # for tdi in transition_df.index:
-    #     i1, i2 = list(tdi)
-    #     transition_df.loc[tdi, 'rate'] = tm_rate[int(i1) - 1, int(i2) - 1]
-
-    transition_df.loc[tb, 'rate'] = transition_df.loc[tb, 'nb_transitions'] / transition_df.loc[tb, 'nb_samples'] * framerate
+    tm_rate = np.eye(nb_classes) + framerate * logm(tm)
+    tm_rate[tm_rate<0] = 0.0
+    for tdi in transition_df.index:
+        i1, i2 = [int(ii) for ii in tdi.split('_')]
+        transition_df.loc[tdi, 'rate'] = tm_rate[int(i1) - 1, int(i2) - 1]
 
     # Loaded from FRETboard
     tr_df_list = []
@@ -460,6 +462,7 @@ if tr_files:
     for cat in args.categories:
         fig = plot_transition_bubble(transition_piv_df, cat)
         fig.savefig(f'{summary_dir}/transition_bubbleplot_{cat}.svg')
+        plt.close()
 
 # plot correct E_FRET histograms
 eventstats_df = pd.concat(eventstats_list)

not_for_upload/plotting_functions.py
@@ -131,6 +131,9 @@ def plot_transition_bar(transition_df, yerr, ax):
     ax.set_ylim(bottom=0)
 
 def plot_transition_bubble(tdf, cat):
+    tdf = tdf.copy()
+    tdf.loc[tdf.loc[:, cat] < 0.001, cat] = 0
+    tdf.loc[tdf.loc[:, 'actual'] < 0.001, 'actual'] = 0
     pts_scaling = 7500
     fig = plt.figure()
 
@@ -159,8 +162,8 @@ def plot_transition_bubble(tdf, cat):
 
     # make legend, from: https://blogs.oii.ox.ac.uk/bright/2014/08/12/point-size-legends-in-matplotlib-and-basemap-plots/
     max_marker_size = tdf.loc[:, [cat, 'actual']].max().max()
-    labels_raw = np.arange(0.05, 0.5, 0.05)
-    labels = [0.05] + [labels_raw[li].round(2) for li in range(1, len(labels_raw)) if
+    labels_raw = np.arange(0.01, 0.2, 0.01)
+    labels = [0.01] + [labels_raw[li].round(2) for li in range(1, len(labels_raw)) if
                        labels_raw[li - 1] <= max_marker_size]
     lab_obj_list = [plt.scatter([], [], s=l * pts_scaling, edgecolors='none',
                                 color='grey') for l in labels]